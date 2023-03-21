12f906e91adfbb145f600971ce7a1d6f62905f19
Laura <laurakovacic@outlook.com>
2023-03-21 08:27:03 +0100

    initial commit

---
+++ src/test/java/com/laurakovacic/spring6demowebapp/Spring6DemoWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurakovacic.spring6demowebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6DemoWebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
